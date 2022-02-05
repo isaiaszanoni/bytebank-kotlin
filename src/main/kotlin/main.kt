dd9fc5e540a54416a24444fbe21f428bb23312e1
isaiaszanoni <isaiasz.mus@gmail.com>
2022-02-04 23:39:17 -0300

    feat(main) Implementacao de classe conta e metodo depositar

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,24 @@
+fun main() {
+    var conta1 = Conta()
+    conta1.titular = "Pedro"
+    println(conta1.titular)
+
+    var contaFran = Conta()
+    contaFran.titular = "Fran"
+    contaFran.numero = 1234
+    contaFran.saldo += 2000.0
+    //println("A cliente ${contaFran.titular} possui R$${contaFran.saldo} em saldo.")
+    depositar(contaFran, 50.0)
+    println(contaFran.saldo)
+}
+
+class Conta(){
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+}
+
+fun depositar(conta: Conta, valor: Double) {
+    conta.saldo += valor
+}
+
